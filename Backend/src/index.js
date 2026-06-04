2493c08f5820b9170b1f7be57ecb180188ad05b8
LokTantra15 <loktantra.team@gmail.com>
2026-06-04 19:38:53 +0530

    updated cors url

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173' }));
+app.use(cors({ origin: process.env.CLIENT_URL || 'https://github-repo-explorer-ruchi.vercel.app' }));
 app.use(express.json());
 
 // Routes
